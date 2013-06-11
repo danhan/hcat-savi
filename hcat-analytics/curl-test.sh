69d308870b263d3a0ab3270c2bafc72409c4031b
Ubuntu <ubuntu@core1.(none)>
2013-06-11 19:29:25 +0000

    add curl testing scripts

---
+++ hcat-analytics/curl-test.sh
@@ -0,0 +1,4 @@
+curl -H "Content-Type: application/json" -X POST -d '{ "start-time":"2012-01-01 01:00:00", "regions":"localhost", "end-time":"2013-01-01 01:00:00", "location":"40,40", "distance":1000 }' http://core1:9999/analytics/spatial/range
+
+
+
