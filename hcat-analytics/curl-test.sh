6aae0904a380cbf6e14d429a6e54cb55cf9ad898
Ubuntu <ubuntu@core1.(none)>
2013-06-13 00:00:10 +0000

    add one more query

--- hcat-analytics/curl-test.sh
+++ hcat-analytics/curl-test.sh
@@ -2,11 +2,11 @@
 
 
 echo "===The result of Range==="
-curl -H "Content-Type: application/json" -X POST -d '{ "start-time":"2012-01-01 01:00:00", "regions":"localhost", "end-time":"2013-01-01 01:00:00", "location":"40,40", "distance":1000 }' http://core1:9999/analytics/spatial/range
+curl -H "Content-Type: application/json" -X POST -d '{ "start-time":"2012-01-01 01:00:00", "regions":"ab", "end-time":"2013-01-01 01:00:00", "location":"40,40", "distance":1000 }' http://core1:9999/analytics/spatial/range
 
 echo ""
 echo "===The result of Window==="
 
-curl -H "Content-Type: application/json" -X POST -d '{ "start-time":"2012-01-01 01:00:00", "end-time":"2013-01-01 01:00:00", "regions":"localhost", "areas":["NW","NE","SW","SE"] }' http://core1:9999/analytics/spatial/window
+curl -H "Content-Type: application/json" -X POST -d '{ "start-time":"2012-01-01 01:00:00", "end-time":"2013-01-01 01:00:00", "regions":"ab", "areas":["NW","NE","SW","SE"] }' http://core1:9999/analytics/spatial/window
 
 echo ""
