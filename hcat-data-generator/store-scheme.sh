1b3e6b92fe22db3e77b0851306ea5fa63e96bc71
Ubuntu <ubuntu@bcedge.(none)>
2013-06-21 15:52:18 +0000

    store the schema

--- hcat-data-generator/store-scheme.sh
+++ hcat-data-generator/store-scheme.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+
+username=root
+password=zhu88jie
+
+echo "drop database"
+mysql -u$username -p$password -e "drop database hcaschedule"
+
+echo "create database"
+mysql -u$username -p$password -e "create database hcaschedule"
+
+echo "store the schema to hcaschedule"
+mysql -u$username -p$password hcaschedule < hcat-db-schema.sql
