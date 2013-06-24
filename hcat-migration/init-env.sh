d231f1f855f5d94574b0ace0978ff5fd19bdb6be
Ubuntu <ubuntu@oozie.(none)>
2013-06-24 00:32:53 +0000

    change lib and numbers

--- hcat-migration/init-env.sh
+++ hcat-migration/init-env.sh
@@ -1,3 +1,3 @@
-hadoop dfs -rmr /user/ubuntu/oozie
+hadoop dfs -rmr /user/ubuntu/share
 hadoop dfs -mkdir /user/ubuntu/share
 hadoop dfs -put ./lib/ /user/ubuntu/share/
--- sqoop-ext/copy-to-migration.sh
+++ sqoop-ext/copy-to-migration.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 cp ./bin/sqoop-ext.jar ../hcat-migration/src/oozie/sub-workflow/
+cp ./bin/sqoop-ext.jar /usr/local/sqoop/lib
