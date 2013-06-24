fcf62ce9958415ef0ea35427f49b98433d556eb1
Dan Han <dhan3@ualberta.ca>
2013-06-24 11:38:16 -0600

    add patient,patientaddress, and hca

--- hcat-data-generator/batch-upload.sh
+++ hcat-data-generator/batch-upload.sh
@@ -4,6 +4,45 @@ username=root
 password=passw0rd
 data_dir=/home/dan/Downloads/test-data
 
+
+cd $data_dir/hca
+tablename=HCA
+echo "======start to upload $tablename===="
+for f in *.csv
+do
+    echo $f
+    mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
+done
+# you can uncomment it if the space is limited
+#echo "delete the $data_dir/hca dataset"
+#rm -rf $data_dir/hca
+
+
+cd $data_dir/patient
+tablename=Patient
+echo "========start to upload $tablename======="
+for f in *.csv
+do
+    echo $f
+    mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
+done
+# you can uncomment it if the space is limited
+#echo "delete the $data_dir/patient dataset"
+#rm -rf $data_dir/patient
+
+cd $data_dir/patientaddress
+tablename=PersistentAddress
+echo "=======start to upload $tablename========"
+for f in *.csv
+do
+    echo $f
+    mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
+done
+# you can uncomment it if the space is limited
+#echo "delete the $data_dir/patientaddress dataset"
+#rm -rf $data_dir/patientaddress
+
+
 cd $data_dir/service
 tablename=Service
 echo "start to upload $tablename"
@@ -15,6 +54,19 @@ done
 # you can uncomment it if the space is limited
 #echo "delete the $data_dir/service"
 #rm -rf $data_dir/serivce
+
+
+cd $data_dir/schedule
+tablename=Schedule
+echo "start to upload $tablename"
+for f in *.csv
+do
+    echo $f
+    mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
+done
+# you can uncomment it if the space is limited
+#echo "delete the $data_dir/schedule dataset"
+#rm -rf $data_dir/schedule
 
 
 cd $data_dir/appointment
@@ -42,6 +94,15 @@ done
 #echo "delete the $data_dir/schedule dataset"
 #rm -rf $data_dir/schedule
 
+cd $data_dir/servicerecord
+tablename=ServiceRecord
+echo "start to upload $tablename"
+for f in *.csv
+do
+    echo $f
+    mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
+done
+
 
 cd $data_dir/media
 tablename=MediaMessage
@@ -57,42 +118,3 @@ done
 #rm -rf $data_dir/media
 
 
-cd $data_dir/servicerecord
-tablename=ServiceRecord
-echo "start to upload $tablename"
-for f in *.csv
-do
-    echo $f
-    mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
-done
-
-# you can uncomment it if the space is limited
-#echo "delete the $data_dir/servicerecord dataset"
-#rm -rf $data_dir/servicerecord
-
-cd $data_dir/patient
-tablename=Patient
-echo "start to upload $tablename"
-for f in *.csv
-do
-    echo $f
-    mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
-done
-
-# you can uncomment it if the space is limited
-#echo "delete the $data_dir/patient dataset"
-#rm -rf $data_dir/patient
-
-
-cd $data_dir/hca
-tablename=HCA
-echo "start to upload $tablename"
-for f in *.csv
-do
-   echo $f
-   mysql --local-infile -u$username -p$password hcaschedule -e "LOAD DATA LOCAL INFILE '"$f"' INTO TABLE $tablename FIELDS TERMINATED BY ','"
-done
-
-# you can uncomment it if the space is limited
-#echo "delete the $data_dir/hca dataset"
-#rm -rf $data_dir/hca
