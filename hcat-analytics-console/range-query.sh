b04d6e8efc3ac8649b3529512c6214c37ec5c6f4
Dan Han <dhan3@ualberta.ca>
2013-06-27 10:58:30 -0600

    format the output

--- hcat-analytics-console/range-query.sh
+++ hcat-analytics-console/range-query.sh
@@ -1,5 +1,20 @@
+#!/bin/bash
 
-echo "===The result of Range==="
-curl -H "Content-Type: application/json" -X POST -d '{ "regions":"localhost","location":"-134,40", "distance":10 }' http://localhost:9999/analytics/spatial/range
+jsondata="{ 'regions':'localhost','location':'-134,40', 'distance':20 }"
+url="http://localhost:9999/analytics/spatial/range"
+
+printf "===Spatial Range Request with CURL=========\n"
+
+result=`curl -H "Content-Type: application/json" -X POST -d "$jsondata" $url`
+
+printf "\n************************************************\n"
+printf "** The result of Range query $jsondata \n"
+printf "*************************************************\n"
+
 echo ""
-echo "============finished=============="
+echo $result | python -mjson.tool
+echo ""
+
+printf "\n************************************************\n"
+printf "************Finish the request*******************\n"
+printf "*************************************************\n"
