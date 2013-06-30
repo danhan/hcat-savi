59977d46e5c0201d41c6026cbd20359004cd547c
Dan Han <dhan3@ualberta.ca>
2013-06-30 12:18:13 -0600

    refine the query

--- hcat-analytics-console/console.sh
+++ hcat-analytics-console/console.sh
@@ -6,15 +6,17 @@ query_start_time="2010-01-01 01:00:00"
 query_end_time="2013-12-31 18:00:00"
 query_one_region='bc'
 query_regions='localhost,bc'
+query_service_condition=''
+query_media_condition=''
 
 
 function send_request(){
 
-    printf "===Spatial Range Request with CURL=========\n"
+    printf "=== Request with CURL=========\n"
     result=`curl -H "Content-Type: application/json" -X POST -d "$1" $2`
 
     printf "\n************************************************\n"
-    printf "** The result of Range query $1 \n"
+    printf "** The result of query $1 \n"
     printf "*************************************************\n"
     echo $result | python -mjson.tool
     printf "\n************************************************\n"
@@ -43,12 +45,12 @@ stat_sum_url=$url"/statistics/sum"
 send_request "$appointment" $stat_sum_url
 
 elif [ "$1" == "4" ]; then
-service_record="{ 'numerator': 'service', 'object': 'record', 'condition':'1', 'start-time':'$query_start_time', 'end-time':'$query_end_time', 'unit':'m', 'regions':'$query_regions'}"
+service_record="{ 'numerator': 'service', 'object': 'record', 'condition':'$query_service_condition', 'start-time':'$query_start_time', 'end-time':'$query_end_time', 'unit':'m', 'regions':'$query_regions'}"
 stat_pct_url=$url"/statistics/pct"
 send_request "$service_record" $stat_pct_url
 
 elif [ "$1" == "5" ]; then
-media="{ 'numerator': 'media', 'object': 'record', 'condition':'0', 'start-time':'$query_start_time', 'end-time':'$query_end_time', 'unit':'m', 'regions':'$query_regions'}"
+media="{ 'numerator': 'media', 'object': 'record', 'condition':'$query_media_condition', 'start-time':'$query_start_time', 'end-time':'$query_end_time', 'unit':'m', 'regions':'$query_regions'}"
 stat_pct_url=$url"/statistics/pct"
 send_request "$media" $stat_pct_url
 fi
