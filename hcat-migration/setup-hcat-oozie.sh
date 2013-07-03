4d85cab828236b9752b6b55f34038f54e2bb2013
Ubuntu <ubuntu@zookeeper01.local>
2013-07-03 14:15:59 +0000

    change to be more flexible

--- hcat-migration/setup-hcat-oozie.sh
+++ hcat-migration/setup-hcat-oozie.sh
@@ -1,3 +1,9 @@
-hadoop dfs -rmr /user/ubuntu/oozie
-hadoop dfs -put ./src/oozie /user/ubuntu/oozie
-hadoop dfs -ls /user/ubuntu/oozie
+#!/bin/bash
+
+#ubuntu in Nebula, oozie in SAVI
+oozie_user='ubuntu'
+
+hadoop dfs -rmr /user/$oozie_user/hcat/
+#hadoop dfs -put ./src/oozie /user/$oozie_user/hcat
+hadoop dfs -put ./src/savi /user/$oozie_user/hcat
+hadoop dfs -ls /user/$oozie_user/hcat
--- hcat-migration/submit-hcat-oozie.sh
+++ hcat-migration/submit-hcat-oozie.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+oozie_ip='10.12.7.33'
 
-$OOZIE_HOME/bin/oozie job -oozie http://oozie:11000/oozie -config ./src/oozie/coordinator.properties -run
+#$OOZIE_HOME/bin/oozie job -oozie http://$oozie_ip:11000/oozie -config ./src/oozie/coordinator.properties -run
+
+
+/usr/bin/oozie job -oozie http://$oozie_ip:11000/oozie -config ./src/savi/coordinator.properties -run
