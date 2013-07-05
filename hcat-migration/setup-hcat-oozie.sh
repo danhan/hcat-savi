11111621617ff96f75264545c8f34f8a6c5f1f3d
Ubuntu <ubuntu@zookeeper02.local>
2013-07-05 03:52:05 +0000

    this is for savi

--- hcat-migration/setup-hcat-oozie.sh
+++ hcat-migration/setup-hcat-oozie.sh
@@ -3,7 +3,7 @@
 #ubuntu in Nebula, oozie in SAVI
 oozie_user='ubuntu'
 
-hadoop dfs -rmr /user/$oozie_user/hcat/
+hadoop dfs -rmr /user/$oozie_user/hcat
 #hadoop dfs -put ./src/oozie /user/$oozie_user/hcat
 hadoop dfs -put ./src/savi /user/$oozie_user/hcat
 hadoop dfs -ls /user/$oozie_user/hcat
--- hcat-migration/submit-hcat-oozie.sh
+++ hcat-migration/submit-hcat-oozie.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-oozie_ip='10.12.7.33'
+oozie_ip='10.12.7.31'
 
-#$OOZIE_HOME/bin/oozie job -oozie http://$oozie_ip:11000/oozie -config ./src/oozie/coordinator.properties -run
+$OOZIE_HOME/bin/oozie job -oozie http://$oozie_ip:11000/oozie -config ./src/oozie/coordinator.properties -run
 
 
-/usr/bin/oozie job -oozie http://$oozie_ip:11000/oozie -config ./src/savi/coordinator.properties -run
