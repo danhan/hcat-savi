80cc4760a6a98251829c7f0487ec049464f7b696
Ubuntu <ubuntu@oozie.(none)>
2013-07-05 06:32:37 +0000

    change back from savi

--- hcat-migration/submit-hcat-oozie.sh
+++ hcat-migration/submit-hcat-oozie.sh
@@ -1,6 +1,2 @@
-#!/bin/bash
-oozie_ip='10.12.7.31'
 
-$OOZIE_HOME/bin/oozie job -oozie http://$oozie_ip:11000/oozie -config ./src/oozie/coordinator.properties -run
-
-
+$OOZIE_HOME/bin/oozie job -oozie http://oozie:11000/oozie -config ./src/oozie/coordinator.properties -run
--- sqoop-ext/copy-to-migration.sh
+++ sqoop-ext/copy-to-migration.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-#rm -rf ../hcat-migration/src/oozie/sub-workflow/sqoop-ext.jar
-#cp ./bin/sqoop-ext.jar ../hcat-migration/src/oozie/sub-workflow/
-#ls -l ../hcat-migration/src/oozie/sub-workflow/sqoop-ext.jar
+rm -rf ../hcat-migration/src/oozie/sub-workflow/sqoop-ext.jar
+cp ./bin/sqoop-ext.jar ../hcat-migration/src/oozie/sub-workflow/
+ls -l ../hcat-migration/src/oozie/sub-workflow/sqoop-ext.jar
 
-# this is for savi
-rm -rf ../hcat-migration/src/savi/sub-workflow/sqoop-ext.jar
-cp ./bin/sqoop-ext.jar ../hcat-migration/src/savi/sub-workflow/
-ls -l ../hcat-migration/src/savi/sub-workflow/sqoop-ext.jar
